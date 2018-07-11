0f78dd4367528ec41a980c5915bee3c434521dfd
Student <student@Admins-MacBook-Air-9.local>
2018-07-10 17:02:54 -0700

    update

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,41 @@
-// Author: FirstName LastName
+// Author: Jonathan Rodriguez
+
+let readline = require('readline-sync');
+
+console.log('*************************************************************************');
+
+console.log('WeLcOmE TO thE ChAnGe MaChIne!!');
+
+let cents = Number(readline.question ('Please Enter cents as a positive integer'));
+
+console.log(cents + 'Cents Makes');
+
+let quarters = Math.floor(cents / 25);
+
+cents = cents % 25;
+
+let dimes = Math.floor(cents / 10);
+
+cents = cents % 10;
+
+let nickles = Math.floor(cents / 5);
+
+cents = cents % 5;
+
+let pennies = Math.floor(cents / 1);
+
+cents = cents % 1;
+
+console.log('Quarters' + quarters);
+
+console.log('Dimes' + dimes);
+
+console.log('Nickles' + nickles);
+
+console.log('Pennies' + pennies);
+
+console.log('hOpE yOu ArE sAtisFied wItH YoUR rEsulTS');
+
+console.log('HAVE A GOOD DAY GOODBYE');
+
+console.log('*************************************************************************')
--- hello.js
+++ hello.js
@@ -1,3 +1,12 @@
 // Author: Jonathan Rodriguez
 
 //console.log("y = 2003")
+
+let readline = require('readline-sync');
+
+let age = Number(readline.question('What is your age?'));
+
+//let age
+//let age = Number(ageAsAString);
+
+console.log('In 2028 you will be ' + (age + 10) + 'years old');
